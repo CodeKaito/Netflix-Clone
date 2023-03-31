96d0aff396f70e56297222fb2cd98a1ce8f100f9
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-03-31 10:56:41 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,31 +1,32 @@
 import React, { useEffect } from 'react';
-import HomeScreen from './screens/HomeScreen';
+
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { auth } from './firebase';
 
-import './App.css'
-import LoginScreen from './screens/LoginScreen';
 import { useDispatch, useSelector } from 'react-redux';
 import { login, logout, selectUser } from './features/userSlice';
 import ProfileScreen from './screens/ProfileScreen';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import './App.css'
 
 function App() {
   const user = useSelector(selectUser);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+    const unsubscribe = auth.onAuthStateChanged((userAuth => {
       if (userAuth) {
 
         dispatch(login({
           uid: userAuth.uid,
-          email: userAuth.email
-        }))
+          email: userAuth.email,
+        }));
       } else {
         dispatch(logout);
       }
-    });
-    return unsubscribe();
+    }));
+    return unsubscribe;
   }, []);
   
 
