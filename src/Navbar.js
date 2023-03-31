d8c9c2d1c2c95965f4f3576138fc6fec6337d99a
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-03-31 11:25:59 +0200

    update

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,10 +1,11 @@
 import './Navbar.css'
 import React, { useState, useEffect } from 'react'
-import { useHistory } from 'react-router-dom';
+import { createBrowserHistory } from "history"
 
 const NavBar = () => {
+
     const [show, handleShow] = useState(false);
-    const history = useHistory();
+    const history = createBrowserHistory();
 
     const transitionNavbar = () => {
         if (window.scrollY > 100) {
@@ -23,7 +24,7 @@ const NavBar = () => {
   return (
     <div className={`nav ${show && "nav__black"}`}>
         <div className="nav__contents">
-            <img className='nav__logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt='netflix' />
+            <img onClick={() => history.push("/")} className='nav__logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt='netflix' />
             <img onClick={() => history.push("/profile")} className='nav__avatar' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt='' /> 
         
         </div>
--- src/screens/ProfileScreen.js
+++ src/screens/ProfileScreen.js
@@ -1,9 +1,28 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+import { selectUser } from '../features/userSlice'
+import NavBar from '../Navbar'
 import './ProfileScreen.css'
 
 const ProfileScreen = () => {
+  const user = useSelector(selectUser);
+
   return (
-    <div>ProfileScreen</div>
+    <div className='profileScreen'>
+      <NavBar />
+      <div className='profileScreen__body'>
+        <h1>Edit Profile</h1>
+        <div className="profileScreen__info">
+          <img src="https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png" alt="netflix_avatar" />
+          <div className="profileScreen__details">
+            <h2>{user.email}</h2>
+            <div className="profileScreen__plans">
+              <button className='profileScreen__signOut'>Sign Out</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
