22976257b47942f5f562412ffbb503d162340914
Jerome Decinco <jerome.decinco@maxwell.mi.it>
2023-03-31 11:10:49 +0200

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,8 +1,10 @@
 import './Navbar.css'
 import React, { useState, useEffect } from 'react'
+import { useHistory } from 'react-router-dom';
 
 const NavBar = () => {
     const [show, handleShow] = useState(false);
+    const history = useHistory();
 
     const transitionNavbar = () => {
         if (window.scrollY > 100) {
@@ -22,7 +24,7 @@ const NavBar = () => {
     <div className={`nav ${show && "nav__black"}`}>
         <div className="nav__contents">
             <img className='nav__logo' src='https://assets.stickpng.com/images/580b57fcd9996e24bc43c529.png' alt='netflix' />
-            <img className='nav__avatar' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt='' /> 
+            <img onClick={() => history.push("/profile")} className='nav__avatar' src='https://upload.wikimedia.org/wikipedia/commons/0/0b/Netflix-avatar.png' alt='' /> 
         
         </div>
         
